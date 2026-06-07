2adc44603db7f1b6984c6694cbf586cc10f39f3d
melodyBee <abeerafatimarub@gmail.com>
2026-06-07 22:18:07 +0500

    change of version

--- sw.js
+++ sw.js
@@ -1,31 +1,29 @@
-const CACHE = 'cocora-v1';
+const CACHE = 'cocora-v2'
 const ASSETS = [
-  './',
-  './index.html',
-  './script.js',
-  './style.css',
-  './manifest.json',
-  './assets/icon.png',
-];
+    './',
+    './index.html',
+    './script.js',
+    './style.css',
+    './manifest.json',
+    './assets/icon.png',
+]
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)))
+    self.skipWaiting()
+})
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches
+            .keys()
+            .then((keys) =>
+                Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+            )
     )
-  );
-  self.clients.claim();
-});
+    self.clients.claim()
+})
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)))
+})
